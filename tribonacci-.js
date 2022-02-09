f1be1b1f28299269181eae3a856f33bf01b4bf25
michelleroos <hello.michelleroos@gmail.com>
2022-02-09 10:17:47 -0500

    trib

--- tribonacci-.js
+++ tribonacci-.js
@@ -6,6 +6,9 @@ The 0-th and 1-st numbers of the sequence are both 0.
 
 The 2-nd number of the sequence is 1.
 
+trib 0 0 1 1 2
+idx  0 1 2 3 4 5 6 7 8 
+
 To generate further numbers of the sequence, calculate the sum of previous three numbers.
 
 Solve this recursively.
@@ -19,6 +22,11 @@ const tribonacci = (n, memo = {}) => {
   return memo[n];
 };
 
+/*
+Runtime: 97 ms, faster than 35.58% of JavaScript online submissions for N-th Tribonacci Number.
+Memory Usage: 42.3 MB, less than 5.50% of JavaScript online submissions for N-th Tribonacci Number.
+*/
+
 console.log(tribonacci(0)); 
 // -> 0
 
@@ -27,6 +35,9 @@ console.log(tribonacci(1));
 
 console.log(tribonacci(2)); 
 // -> 1
+
+console.log(tribonacci(4)); 
+// -> 4
 
 console.log(tribonacci(5)); 
 // -> 4
